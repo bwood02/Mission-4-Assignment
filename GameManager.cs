1f38a0197e5da657cdc3bad16c1d3024089a22fe
Brandon Wood <bwood02@byu.edu>
2026-01-28 10:42:49 -0700

    Added the GameResults method to check each possible pattern, if any winning pattern has the same character in all 3 spots, return that character as the winner (otherwise return an empty char ' ')

--- GameManager.cs
+++ GameManager.cs
@@ -6,13 +6,13 @@ namespace Mission_4_Assignment
 {
     internal class GameManager
     {
-        public void PrintBoard(char[] gameBoard)
+        public void PrintgameBoard(char[] gamegameBoard)
         {
-            Console.WriteLine("Current Board:");
-            for (int i = 0; i < gameBoard.Length; i++)
+            Console.WriteLine("Current gameBoard:");
+            for (int i = 0; i < gamegameBoard.Length; i++)
             {
                 // Print value with spaces
-                Console.Write(" " + gameBoard[i] + " ");
+                Console.Write(" " + gamegameBoard[i] + " ");
 
                 // Print vertical bars (not at end of row)
                 if ((i + 1) % 3 != 0)
@@ -33,12 +33,41 @@ namespace Mission_4_Assignment
                 }
             }
         }
+        public char GameResults(char[] gameBoard)
+        {
+            char winner = ' '; // No winner yet
 
-        // PrintBoard(char[] gameBoard);
+            // Array of arrays: each inner array is a winning combination
+            int[][] winPatterns = new int[][] // "new" creates the array object in memory
+                {
+                    new int[] {0,1,2}, // top
+                    new int[] {3,4,5}, // middle
+                    new int[] {6,7,8}, // bottom 
+                    new int[] {0,3,6}, // left
+                    new int[] {1,4,7}, // middle
+                    new int[] {2,5,8}, // right
+                    new int[] {0,4,8}, // top-left → bottom-right
+                    new int[] {2,4,6}  // top-right → bottom-left
+                };
 
-        //public char GameResults(char[] gameBoard)
-        //{
+                // Loop through each winning pattern
+                foreach (int[] pattern in winPatterns)
+                {
+                    // Create a, b, c variables for each position in the winning pattern
+                    int a = pattern[0]; // first index in int[ ] pattern above
+                    int b = pattern[1]; // second index
+                    int c = pattern[2]; // third index
 
-        //}
+                    // Check if all 3 positions have the same non-empty value (from the gameBoard array)
+                    if (gameBoard[a] == gameBoard[b] &&
+                        gameBoard[b] == gameBoard[c] &&
+                        gameBoard[a] != ' ')
+                    {
+                        winner = gameBoard[a]; // If they are all the same, return that value ('X' or 'O')
+                    }
+                }
+
+                return winner; // No winner yet
+            }
     }
 }
